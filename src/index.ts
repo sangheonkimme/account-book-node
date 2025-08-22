3200ef079fbcc61b9ab43b89da5e14c5a31c1c02
sangheonkimme <sangheon1646@gmail.com>
2025-08-22 17:06:56 +0900

    feat : DB 연결 확인 로직 추가

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import authRoutes from './routes/auth.routes';
-import accountBookRoutes from './routes/accountBook.routes';
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes";
+import accountBookRoutes from "./routes/accountBook.routes";
+import { prisma } from "./prisma/client";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,9 +10,24 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/auth', authRoutes);
-app.use('/account-book', accountBookRoutes);
+app.use("/auth", authRoutes);
+app.use("/account-book", accountBookRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const checkDBConnection = async () => {
+  try {
+    await prisma.$connect();
+    console.log("✅ MySQL DB 연결 성공!");
+  } catch (error) {
+    console.error("❌ MySQL DB 연결 실패:", error);
+    process.exit(1);
+  }
+};
+
+const startServer = async () => {
+  await checkDBConnection();
+  app.listen(port, () => {
+    console.log(`🚀 서버 실행 중 : ${port}`);
+  });
+};
+
+startServer();
